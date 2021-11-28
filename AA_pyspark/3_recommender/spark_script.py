9e9489dd55172b89bedde44723fc19349ff1d8fc
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-11-28 08:04:41 -0800

    recommender++

--- AA_pyspark/3_recommender/spark_script.py
+++ AA_pyspark/3_recommender/spark_script.py
@@ -1,4 +1,6 @@
 from psyspark.ml.recommendation import ALS
+from pyspark.sql.functions import col, split, min, max
+from pyspark.sql.types import IntegerType, StringType
 
 
 DATA = '../../data/audioscrobbler'
@@ -22,3 +24,45 @@ raw_artist_alias.show(5)
 #mod = als.fit(train)
 #predictions = model.transform(test)
 
+
+user_artist_df = (raw_user_artist_data
+                  .withColumn('user', split(raw_user_artist_data['value'], ' ')
+                              .getItem(0)
+                              .cast(IntegerType()))
+                  .withColumn('artist', split(raw_user_artist_data['value'], ' ')
+                              .getItem(1)
+                              .cast(IntegerType()))
+                  .withColumn('count', split(raw_user_artist_data['value'], ' ')
+                              .getItem(2)
+                              .cast(IntegerType()))
+                  .drop('value'))
+user_artist_df.select(
+    [min('user'), max('user'), min('artist'), max('artist')]
+).show()
+
+artist_by_id = (raw_artist_data
+                .withColumn('id', split(col('value'), '\s+', 2)
+                            .getItem(0)
+                            .cast(IntegerType()))
+                .withColumn('name', split(col('value'), '\s+', 2)
+                            .getItem(1)
+                            .cast(StringType()))
+                .drop('value'))
+artist_by_id.show(5)
+
+artist_alias = (raw_artist_alias
+                .withColumn('artist', split(col('value'), '\s+')
+                            .getItem(0)
+                            .cast(IntegerType()))
+                .withColumn('alias', split(col('value'), '\s+')
+                            .getItem(1)
+                            .cast(StringType()))
+                .drop('value'))
+artist_alias.show(5)
+
+# Example lookup
+artist_by_id.filter(artist_by_id.id.isin(1092764, 1000311)).show()
+
+
+
+# Building an Initial Model
