05e6af33fdf4039c9b4750e5fa9e7d0a154ebdcc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-11-30 14:56:42 -0800

    recommnder++

--- AA_pyspark/3_recommender/spark_script.py
+++ AA_pyspark/3_recommender/spark_script.py
@@ -25,39 +25,41 @@ raw_artist_alias.show(5)
 #predictions = model.transform(test)
 
 
-user_artist_df = (raw_user_artist_data
-                  .withColumn('user', split(raw_user_artist_data['value'], ' ')
-                              .getItem(0)
-                              .cast(IntegerType()))
-                  .withColumn('artist', split(raw_user_artist_data['value'], ' ')
-                              .getItem(1)
-                              .cast(IntegerType()))
-                  .withColumn('count', split(raw_user_artist_data['value'], ' ')
-                              .getItem(2)
-                              .cast(IntegerType()))
-                  .drop('value'))
+user_artist_df = (
+    raw_user_artist_data
+    .withColumn(
+        'user',
+        split(raw_user_artist_data['value'], ' ').getItem(0).cast(IntegerType())
+    ).withColumn(
+        'artist',
+        split(raw_user_artist_data['value'], ' ').getItem(1).cast(IntegerType())
+    ).withColumn(
+        'count',
+        split(raw_user_artist_data['value'], ' ').getItem(2).cast(IntegerType())
+    ).drop('value'))
 user_artist_df.select(
     [min('user'), max('user'), min('artist'), max('artist')]
 ).show()
 
-artist_by_id = (raw_artist_data
-                .withColumn('id', split(col('value'), '\s+', 2)
-                            .getItem(0)
-                            .cast(IntegerType()))
-                .withColumn('name', split(col('value'), '\s+', 2)
-                            .getItem(1)
-                            .cast(StringType()))
-                .drop('value'))
+artist_by_id = (
+    raw_artist_data
+    .withColumn(
+        'id',
+        split(col('value'), '\s+', 2).getItem(0).cast(IntegerType()))
+    .withColumn(
+        'name',
+        split(col('value'), '\s+', 2).getItem(1).cast(StringType()))
+    .drop('value'))
 artist_by_id.show(5)
-
-artist_alias = (raw_artist_alias
-                .withColumn('artist', split(col('value'), '\s+')
-                            .getItem(0)
-                            .cast(IntegerType()))
-                .withColumn('alias', split(col('value'), '\s+')
-                            .getItem(1)
-                            .cast(StringType()))
-                .drop('value'))
+artist_alias = (
+    raw_artist_alias
+    .withColumn(
+        'artist',
+        split(col('value'), '\s+').getItem(0).cast(IntegerType()))
+    .withColumn(
+        'alias',
+        split(col('value'), '\s+').getItem(1).cast(StringType()))
+    .drop('value'))
 artist_alias.show(5)
 
 # Example lookup
@@ -90,4 +92,25 @@ model = ALS(
 model.userFactors.show(1, truncate=False)
 
 
+
 # Spot check
+user_id = 2093760
+existing_artist_ids = (
+    train_data.filter(train_data.user == user_id).select('artist').collect())
+existing_artist_ids = [i[0] for i in existing_artist_ids]
+artist_by_id.filter(col('id').isin(exisiting_artist_ids)).show()
+
+user_subset = train_data.select('user').weher(col('user') == user_id).distinct()
+top_predictions = model.recommendForUserSubset(user_subset, 5)
+top_predictions.show()
+
+top_predictions_pandas = top_predictions.toPandas()
+print(top_predictions_pandas)
+
+recommended_artist_ids = [i[0]
+                          for i in top_predictions_pandas.recommendations[0]]
+artist_by_id.filter(col('id').isin(recommended_artist_ids)).show()
+
+
+
+# Evaluation
