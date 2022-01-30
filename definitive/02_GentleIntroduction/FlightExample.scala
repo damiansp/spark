c62c5e288e531595e084fdf7725edf55a8cb02a7
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-01-29 20:24:20 -0800

    intro++

--- definitive/02_GentleIntroduction/FlightExample.scala
+++ definitive/02_GentleIntroduction/FlightExample.scala
@@ -5,10 +5,20 @@ object FlightExample {
   val DATA = "../../data"
 
   spark = SparkSession.builder.appName("flights").getOrCreate()
-  val flightData2015 = spark
-                        .read
-                        .option("inferScehma", "true")
-                        .option("header", "true")
-                        .csv(s"$DATA/flight-data/csv/2015-summary.csv"))
+  val data = spark
+    .read
+    .option("inferScehma", "true")
+    .option("header", "true")
+    .csv(s"$DATA/flight-data/csv/2015-summary.csv")
 
+  // DataFrames and SQL
+  data.createOrReplaceTempView("flightData")
+
+  val sqlWay = spark.sql("""
+    SELECT DEST_COUNTRY_NAME, COUNT(1)
+    FROM flightData
+    GROUP BY DEST_COUNTRY_NAME""")
+  val dfWay = data.groupBy('DEST_COUNTRY_NAME).count()
+  println(sqlWay.explain)
+  println(dfWay.explain)
 }
--- definitive/02_GentleIntroduction/flight_example.py
+++ definitive/02_GentleIntroduction/flight_example.py
@@ -4,8 +4,32 @@ from pyspark.sql import SparkSession
 DATA = '../../data'
 
 spark = SparkSession.builder.appName('flights').getOrCreate()
-flight_data_2015 = (spark
-                    .read
-                    .option('inferScehma', 'true')
-                    .option('header', 'true')
-                    .csv(f'{DATA}/flight-data/csv/2015-summary.csv'))
+data = (spark
+        .read
+        .option('inferScehma', 'true')
+        .option('header', 'true')
+        .csv(f'{DATA}/flight-data/csv/2015-summary.csv'))
+print(data.take(3))
+data.sort('count').explain()
+
+spark.conf.set('spark.sql.shuffle.partitions', '5') # default is 200
+print(data.sort('count').take(2))
+
+
+# DataFrames and SQL
+data.createOrReplaceTempView('flight_data')
+sql_way = spark.sql('''
+    SELECT DEST_COUNTRY_NAME, COUNT(1)
+    FROM flight_data
+    GROUP BY DEST_COUNTRY_NAME''')
+
+df_way = data.groupBy('DEST_COUNTRY_NAME').count()
+
+print('sql:\n')
+sql_way.explain()
+print('\ndf:\n')
+df_way.explain()
+
+
+
+
