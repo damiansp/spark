919436a011f0482553df1be939768d30400ccb5d
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-02-27 16:50:29 -0800

    +spark types

--- definitive/04_StructuredAPI/py/snippets/overview.py
+++ definitive/04_StructuredAPI/py/snippets/overview.py
@@ -1,6 +1,13 @@
+from pyspark.sqpl.types import ByteType
+
+
 df = spark.range(500).toDF('number')
 df.select(df['number'] + 10) # executed in Spark, not python
 
 
 # Rows
 spark.range(2).collect() # Row object
+
+
+# Spark types
+b = ByteType()
--- definitive/04_StructuredAPI/sc/snippets/overview.scala
+++ definitive/04_StructuredAPI/sc/snippets/overview.scala
@@ -1,6 +1,13 @@
+import org.apache.spark.sql.types._
+
+
 val df = spark.range(500).toDF("number")
 df.select(df.col("number") + 10) // executed in Spark, not Scala
 
 
 // Rows
 spark.range(2).toDF().collect() // Row object
+
+
+// Spark Types
+val b = ByteType // Scala type
