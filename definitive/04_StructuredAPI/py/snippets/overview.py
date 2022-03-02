7d323e02fdc3febf0735b027c2e87339e4178043
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-03-02 07:34:20 -0800

    +table mgmt

--- definitive/04_StructuredAPI/py/snippets/overview.py
+++ definitive/04_StructuredAPI/py/snippets/overview.py
@@ -2,11 +2,11 @@ from pyspark.sqpl.types import ByteType
 
 
 df = spark.range(500).toDF('number')
-df.select(df['number'] + 10) # executed in Spark, not python
+df.select(df['number'] + 10)  # executed in Spark, not python
 
 
 # Rows
-spark.range(2).collect() # Row object
+spark.range(2).collect()  # Row object
 
 
 # Spark types
--- learning/04_SparkSQLAndDataFrames/py/spark_sql.py
+++ learning/04_SparkSQLAndDataFrames/py/spark_sql.py
@@ -55,3 +55,51 @@ spark.sql('''
  .where('distance > 1000')
  .orderBy('distance', ascending=False)
 ).show(10)
+
+
+# Creating SQL DBs and Tables
+spark.sql('CREATE DATABASE my_db')
+spark.sql('USE my_db')
+
+# Managed table
+spark.sql(
+    'CREATE TABLE my_managed_table ( '
+    'date STRING, delay INT, distance INT, origin STRING, destination STRING)')
+
+# or
+schema = (
+    'date STRING, delay INT, distance INT, origin STRING, destination STRING')
+flights_df = spark.read.csv(data_path, schema=schema)
+flights_df.write.saveAsTable('flights_mngd')
+
+
+# Unmanaged
+spark.sql(
+    f'CREATE TABLE flights ( '
+    f'date STRING, delay INT, distnace INT, origint STRING, desitation STRING) '
+    f'USING csv OPTIONS ( '
+    f'PATH {data_path})')
+
+flights_df.write.option('path', '/tmp/data/flights').saveAsTable('flights')
+
+
+# Views
+sfo_df = spark.sql(
+    "SELECT date, delay, origin, destination "
+    "FROM flights "
+    "WHERE origin = 'SF0'")
+jfk_df = spark.sql(
+    "SELECT date, delay, origin, destination "
+    "FROM flights "
+    "WHERE origin = 'JFK'")
+sfo_df.createOrReplaeGlobalTempView('origin_SFO_global_tmp_view')
+jfk_df.createOrReplaceTempView('origin_JFK_tmp_view')
+
+spark.read.table('origin_JFK_tmp_view')  # or
+spark.sql('SELECT * FROM origin_JFK_tmp_view')
+
+
+# Drop
+spark.catalog.dropGlobalTempView('origin_SFO_global_tmp_view')
+spark.catalog.dropTempView('origin_JFK_tmp_view')
+
