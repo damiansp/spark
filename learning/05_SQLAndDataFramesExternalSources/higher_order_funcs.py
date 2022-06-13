f3db156f7bb76c9274abd43f7f48f906d9932aaf
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-13 13:09:43 -0700

    learning: +higher order funcs

---
+++ learning/05_SQLAndDataFramesExternalSources/higher_order_funcs.py
@@ -0,0 +1,18 @@
+from pyspark.sql.types import ArrayType, IntegerType, StructType
+
+
+schema = StructType([StructField('celsius', ArrayType(IntegerType()))])
+t_list = [[35, 36, 32, 30, 40, 42, 38]], [[31, 32, 34, 55, 56]]
+t_c = spark.createDataFrame(t_list, schema)
+t_c.createOrReplaceTempView('tC')
+t_c.show()
+
+spark.sql(
+    'SELECT celsius, '
+    '  transform(celsius, t -> ((9*t) DIV 5) + 32) AS fahrenheit, '
+    '  filter(celsius, t -> t > 38) AS highs, '
+    '  exists(celsius, t -> t > 38) AS has_highs '
+    'FROM tC'
+).show()
+
+
