ef2e8079d9d0ade9d5351ddfea72e26d3d95b341
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2022-07-11 15:40:19 -0700

    7/ fin efficiency, init large workload

--- learning/07_OptimizingAndTuning/efficiency.py
+++ learning/07_OptimizingAndTuning/efficiency.py
@@ -4,3 +4,5 @@ spark-submit --conf spark.sql.shuffle.partitions=5 \
   --class main.python.chapter7.SparkConfig_7_1 jars/main-python-chapter7_2.12-1.0 jar
 '''
 
+spark.conf.get('spark.sql.shuffle.partitions')
+spark.conf.set('spark.sql.shuffle.partitions', 5)
---
+++ learning/07_OptimizingAndTuning/large_workloads.py
@@ -0,0 +1,6 @@
+'''
+- spark.dynamicAllocation.enabled (true | false)
+- spark.dynamicAllocation.minExecutors (int)
+- spark.dynamicAllocation.maxExecutors (int)
+- spark.dynamicAllocation.schedulerBacklogTimeout ('1m')
+- spark.dynamicAllocation.executorIdleTimeout ('2m')
