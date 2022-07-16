4b15d101330f54ecd51044183abe80ff78baa60e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2022-07-15 22:19:35 -0700

    wrap up udfs

--- definitive/06_DataTypes/udfs.py
+++ definitive/06_DataTypes/udfs.py
@@ -1,4 +1,5 @@
 from pyspark.sql.functions import col, udf
+from pyspark.sql.types import DoubleType
 
 
 udf_example_df = spark.range(5).toDF('n')
@@ -12,3 +13,6 @@ cube(2.)
 cube_udf = udf(cube)
 udf_example_df.select(cube_udf(col('n'))).show()
 udf_example_df.selectExpr('cube(n)').show()
+
+spark.udf.register('cube', cube, DoubleType())
+udf_example_df.selectExpr('cube(n)').show2
