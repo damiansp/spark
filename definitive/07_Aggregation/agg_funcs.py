8782b403e50a71b3b60ca014fcf0a250fd6651d7
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-07-30 11:06:57 -0700

    def/agg funcs++

--- definitive/07_Aggregation/agg_funcs.py
+++ definitive/07_Aggregation/agg_funcs.py
@@ -1,4 +1,6 @@
-from pyspark.sql.functions import count, countDistinct
+from pyspark.sql.functions import (
+    approx_count_distinct, avg, count, countDistinct, expr, first, last,
+    max as sql_max, min as sql_min, sum as sql_sum, sumDistinct)
 
 df = (
     spark
@@ -17,3 +19,18 @@ df.select(countDistinct('StockCode')).show()
 # SELECT COUNT(DISTINCT StockCode) FROM df
 
 
+df.select(approx_count_distinct('StockCode', 0.1)).show()
+df.select(first('StockCode'), last('StockCode')).show()
+df.select(sql_min('Quantity'), sql_max('Quantity')).show()
+df.select(sql_sum('Quantity')).show()
+df.select(sumDistinct('Quantity')).show()
+
+(df
+ .select(
+     count('Quantity').alias('n_transactions'),
+     sql_sum('Quantity').alias('total_sales'),
+     avg('Quantity').alias('mean_sale'),
+     expr('mean(Quantity)').alias('mean_sale_again'))
+ .selectExpr(
+     'total_sales / n_transactions', 'mean_sale', 'mean_sale_again')
+ .show())
