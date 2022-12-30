7c9484aa41a7cbcbe116eee2f7a9c1663d9254a5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-30 11:17:56 -0800

    init mllib w/ ingestion

---
+++ learning/10_MLLib/ingestion_exploration.py
@@ -0,0 +1,10 @@
+path = '/path/to/sf-airbnb-clean.parquet'
+abb_df = spark.read.parquet(path)
+abb_df.select(
+    'neighbouhood cleansed',
+    'room_type',
+    'bedrooms',
+    'bathrooms',
+    'number_of_reviews',
+    'price'
+).show(5)
