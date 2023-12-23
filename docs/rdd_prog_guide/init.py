9a4228c40a51252c6d9f86d873fccf99bb654be6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-23 12:17:35 -0800

    init++

--- docs/rdd_prog_guide/init.py
+++ docs/rdd_prog_guide/init.py
@@ -4,3 +4,8 @@ from pyspark import SparkConf, SparkContext
 # Typically set master in spark-submit
 conf = SparkConf().setAppName('MyApp')#.setMaster('local')
 sc = SparkContext(conf=conf)
+
+data = [1, 2, 3, 4, 5]
+dist_data = sc.parallelize(data)
+
+dist_file = sc.textFile('data.txt')
