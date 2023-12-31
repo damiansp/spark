b82c27bafd14c0d02ae17af1a98df7c52a76f913
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-31 12:09:47 -0800

    creating++

--- learning_pyspark/03_DataFrames/creating.py
+++ learning_pyspark/03_DataFrames/creating.py
@@ -10,6 +10,8 @@ json_str2 = '{"id": "234", "name": "Michael", "age": 22, "eyes": "green"}'
 json_str3 = '{"id": "345", "name": "Simone", "age": 23, "eyes": "blue"}'
 json_rdd = sc.parallelize((json_str1, json_str2, json_str3))
 df = spark.read.json(json_rdd)
+df.show()
 
 # create a temp table
 df.createOrReplaceTempView('swimmers')
+print(spark.sql('SELECT * FROM swimmers').collect())
