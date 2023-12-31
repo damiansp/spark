1d492a2f869ecc32ff733c4a30ea01f060b00abd
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-31 13:44:13 -0800

    fin getting started

--- docs/spark_SQL/getting_started.py
+++ docs/spark_SQL/getting_started.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import Row, SparkSession
+from pyspark.sql.types import StringType, StructField, StructType
 
 
 DATA = '../../data'
@@ -16,10 +17,12 @@ df.select(df['name'], df['age'] + 1).show()
 df.filter(df['age'] >= 21).show()
 df.groupBy('age').count().show()
 
+
 # Make temp table
 df.createOrReplaceTempView('people')
 sql_df = spark.sql('SELECT * FROM people')
 sql_df.show()
+
 
 # Infer schma using reflection
 lines = sc.textFile(f'{DATA}/people.txt')
@@ -32,3 +35,12 @@ teens = spark.sql('SELECT name FROM people WHERE age >= 13 AND age < 20')
 teen_names = teens.rdd.map(lambda p: f'Name: {p.name}').collect()
 for name in teen_names:
     print(name)
+
+# Specify Schema
+schema_headers = ['name', 'age']
+fields = [StructField(f, StringType(), True) for f in schema_headers]
+schema = StructType(fields)
+people = spark.createDataFrame(people, schema)
+people.createOrReplaceView('people')
+res = spark.sql('SELECT name FROM people')
+res.show()
