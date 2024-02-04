18f60a24b7b016d93fc6904137f69382421ff955
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-02-04 08:55:44 -0800

    Pandas grouping

--- docs/spark_SQL/data_sources/arrow_pandas2.py
+++ docs/spark_SQL/data_sources/arrow_pandas2.py
@@ -1,4 +1,6 @@
-from pyspark.sql import SparkSession
+from typing import Iterator
+
+from pyspark.sql import SparkSession, Window
 from pyspark.sql.functions import col, pandas_udf
 from pyspark.sql.types import LongType
 
@@ -40,3 +42,98 @@ print(mult_f(x, x))
 df = spark.createDataFrame(pd.DataFrame(x, columns=['x']))
 df.select(multiply(col('x'), col('x'))).show()
       
+
+#@pandas_udf('long')
+#def calculate(iterator: Iterator[pd.Series]) -> Iterator[pd.Series]:
+#    state = very_expensive_init()
+#    for x in iterator:
+#        yield calculate_with_state(x, state)
+
+#df.select(calculate('value')).show()
+
+
+pdf = pd.DataFrame({'x': [1, 2, 3]})
+df = spark.createDataFrame(pdf)
+
+
+@pandas_udf('long')
+def incr(iterator: Iterator[pd.Serie]) -> Iterator[pd.Series]:
+    for x in iterator:
+        yield x + 1
+
+df.select('x', incr('x')).show()
+
+
+df = spark.createDataFrame(
+    [(1, 1.), (1, 2.), (2, 3.), (2, 5.), (2, 10.)], ('id', 'v'))
+
+
+@pandas_udf('double')
+def mean_udf(v: pd.Series) -> float:
+    return v.mean()
+
+
+df.select(mean_udf(df.v)).show()
+df.grouby('id').agg(mean_udf(df.v)).show()
+w = (
+    Window
+    .partitionBy('id')
+    .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing))
+df.withColumn('mean_v', mean_udf(df.v).over(w)).show()
+
+
+# Pandas Function APIs
+# Grouped Map
+def subtract_mean(pdf: pd.DataFrame) -> pd.DataFrame:
+    v = pdf.v
+    return pdf.assign(v=v - v.mean())
+
+
+(df
+ .groupby('id')
+ .applyInPandas(subtract_mean, schema='id long, v double')
+ .show())
+
+
+# Map
+df = spark.createDataFrame([(1, 21), (2, 30)], ('id', 'age'))
+
+
+def filter_func(iterator: Iterable[pd.DataFrame]) -> Iterable[pd.DataFrame]:
+    for pdf in iterator:
+        yield pdf[pdf.id == 1]
+
+
+df.mapInPandas(filter_func, schema=df.schema).show()
+
+
+# Co-grouped Map
+df1 = spark.createDataFrame(
+    [(20_000_101, 1, 1.),
+     (20_000_101, 2, 2.),
+     (20_000_102, 1, 3.),
+     (20_000_102, 2, 4.)],
+    ('time', 'id', 'v1'))
+df2 = spark.createDataFrame(
+    [(20_000_101, 1, 'x'), (20_000_101, 2, 'y')], ('time', 'id', 'v2'))
+
+
+def merge_ordered(left: pd.DataFrame, right: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge_ordered(left, right)
+
+
+(df1
+ .groupby('id')
+ .cogroup(df2.groupby('id'))
+ .applyInPandas(merge_ordered, schema='time int, id int, v1 double, v2 string')
+ .show())
+
+
+# Arrow Python UDFs
+    
+
+
+
+
+
+
