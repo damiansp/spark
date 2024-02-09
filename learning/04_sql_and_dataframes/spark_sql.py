93e463d7acefb130c426399c04c237eb6d04814e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-02-09 13:44:41 -0800

    load data; sample queries

--- learning/04_sql_and_dataframes/spark_sql.py
+++ learning/04_sql_and_dataframes/spark_sql.py
@@ -3,4 +3,26 @@ from pyspark.sql import SparkSession
 
 DATA = '../../data/databricks-datasets/learning-spark-v2/flights'
 spark = SparkSession.builder.appName('SQL').getOrCreate()
+path = f'{DATA}/departuredelays.csv'
+df = (
+    spark
+    .read.format('csv')
+    .option('inferSchema', 'true')
+    .option('header', 'true')
+    .load(path))
+df.createOrReplaceTempView('delays')
 
+spark.sql(
+    '''SELECT distance, origin, destination
+    FROM delays
+    WHERE distance > 1000
+    ORDER BY distance DESC'''
+).show(10)
+
+spark.sql(
+    '''SELECT date, delay, origin, destination
+    FROM delays
+    WHERE delay > 120 AND origin = 'SFO'
+    ORDER BY delay DESC'''
+).show(10)
+    
