18887428e8f68304d9ddbd0a42e2a8d09558dc87
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-02-18 18:32:05 -0800

    init data sources for dfs/tables

---
+++ learning/04_sql_and_dataframes/data_sources_for_dfs_tables.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+
+# DF Reader
+spark = SparkSession.builder.appName('DFSources').getOrCreate()
+parquet_file = 'my/path/to/myfile.parquet'
+df = spark.read.format('parquet').load(parquet_file)
+# since parquet is default, above can be just
+df = spark.read.load(parquet_file)
+
+csv_dir = 'my/path/to/my_csv_dir/*'
+df = (
+    spark.read.format('csv')
+    .option('inferSchema', 'true')
+    .option('header', 'true')
+    .option('mode', 'PERMISSIVE')
+    .load(my_csv_dir))
+
+json_dir = 'my/path/to/my_json_dir/*'
+df = spark.read.format('json').load(json_dir)
