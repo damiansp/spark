9c94e45d5c588978798a567ff2b9c3789e40a1f0
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-03-09 21:40:40 -0800

    hof exs

--- learning/05_sql_dfs_external_data_sources/higher_order_funcs.py
+++ learning/05_sql_dfs_external_data_sources/higher_order_funcs.py
@@ -1,1 +1,34 @@
-#
+from pyspark.sql import SparkSession
+from pyspark.sql.types import ArrayType, IntegerType, StructField, StructType
+
+
+spark = SparkSession.builder.appName('HOFs').getOrCreate()
+schema = StructType([StructField('celsius', ArrayType(IntegerType()))])
+t_list = [[35, 36, 32, 40, 40, 42, 38]], [[31, 32, 34, -40, 56]]
+t_c = spark.createDataFrame(t_list, schema)
+t_c.createOrReplaceTempView('tC')
+t_c.show()
+
+
+# transform()
+spark.sql(
+    '''SELECT
+      celsius,
+      transform(celsius, t -> ((t * 9) div 5) + 32) AS fahrenheit
+    FROM tC'''
+).show()
+
+
+# filter()
+spark.sql(
+    '''SELECT celsius, filter(celsius, t -> t > 38) AS high
+    FROM tC'''
+).show()
+
+
+# exists()
+spark.sql(
+    '''SELECT celsius, exists(celsius, t -> t = 38) AS threshold
+    FROM tC'''
+).show()
+
