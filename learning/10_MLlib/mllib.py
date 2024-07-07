79b1c40cb70b229004eae1b5f2758c540d68c5ba
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-07-07 13:48:32 -0700

    mllib++

--- learning/10_MLlib/mllib.py
+++ learning/10_MLlib/mllib.py
@@ -1,3 +1,4 @@
+from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import expr
 
@@ -12,3 +13,11 @@ airbnb_df.select(
     'neighbourhood_cleansed', 'room_type', 'bedrooms', 'bathrooms',
     'number_of_reviews', 'price'
 ).show(5)
+
+
+train_df, test_df = airbnb_df.randomSplit([0.8, 0.2], seed=123)
+print(f'Train: {train_df.count()}, Test: {test_df.count()}')
+
+vec_assembler = VectorAssembler(inputCols=['bedrooms'], outputCol='features')
+vec_train_df = vec_assembler.transform(train_df)
+vec_train_df.select('bedrooms', 'features', 'price').show(10)
